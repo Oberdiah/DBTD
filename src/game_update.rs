769b0ad38f2f9688cbfd435eb91d3de858c3cc31
Thomas <32342015+Atoms-Github@users.noreply.github.com>
2022-10-23 13:33:03 +0100

    Wall collision and slidy

--- src/game_update.rs
+++ src/game_update.rs
@@ -1,4 +1,4 @@
-use crate::{GameState, get_my_name, LoadedMap, WorldSquare};
+use crate::{GameState, get_my_name, LoadedMap, Player, WorldSquare};
 use ggez::input::keyboard::KeyCode;
 use cgmath::{EuclideanSpace, InnerSpace, Point2, Vector2};
 use ggez::Context;
@@ -26,10 +26,9 @@ pub fn update_game(loaded_map: &mut LoadedMap, ctx: &mut Context) {
 		player_pos_delta = player_pos_delta.normalize() * game_state.player.speed;
 	}
 
-	let new_player_position = game_state.player.position + player_pos_delta;
-	let new_player_rect = Rect::new(new_player_position.x, new_player_position.y, game_state.player.size.x, game_state.player.size.y);
-
+	let new_player_rect = get_player_rect(&game_state.player, player_pos_delta);
 	let mut should_reset = false;
+
 
 	for (point, square) in game_state.world_grid.iter_mut() {
 		let square_rect = WorldSquare::get_rect(point);
@@ -40,7 +39,15 @@ pub fn update_game(loaded_map: &mut LoadedMap, ctx: &mut Context) {
 			match square {
 				WorldSquare::Air => {}
 				WorldSquare::Wall => {
-					
+					// Now split into x and y, and check again. For wall sliding. (loh)
+					let x_moved_player = get_player_rect(&game_state.player, Vector2::new(player_pos_delta.x, 0.0));
+					let y_moved_player = get_player_rect(&game_state.player, Vector2::new(0.0, player_pos_delta.y));
+					if x_moved_player.overlaps(&square_rect){
+						player_pos_delta.x = 0.0;
+					}
+					if y_moved_player.overlaps(&square_rect){
+						player_pos_delta.y = 0.0;
+					}
 				}
 				WorldSquare::Fire => should_reset = true,
 				WorldSquare::Slime => player_pos_delta *= 0.0,
@@ -67,4 +74,12 @@ pub fn update_game(loaded_map: &mut LoadedMap, ctx: &mut Context) {
 	}
 
 	game_state.player.position += player_pos_delta;
+}
+pub fn get_player_rect(player: &Player, position_delta: Vector2<f32>) -> Rect{
+	 Rect::new(
+		player.position.x + position_delta.x,
+		player.position.y + position_delta.y,
+		player.size.x,
+		player.size.y
+	)
 }
